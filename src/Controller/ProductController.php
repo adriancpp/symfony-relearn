454f00f00c35341dea9139bff28d806f2fdf40d7
adriancpp <adrian.cpp@wp.pl>
2025-09-06 15:46:48 +0200

    install twig and make bundles; controllers and routes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+final class ProductController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/products', name: 'product_index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
     }
 }
