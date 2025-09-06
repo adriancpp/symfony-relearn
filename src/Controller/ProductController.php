6d6f9e9bf4f52eb545390f96ad6f9b0d4bbd3ab6
adriancpp <adrian.cpp@wp.pl>
2025-09-06 17:55:41 +0200

    added deleting

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -84,4 +84,26 @@ final class ProductController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route('/product/{id<\d+>}/delete', name: 'product_delete')]
+    public function delete(Request $request, Product $product, EntityManagerInterface $manager): Response
+    {
+        if ($request->isMethod('POST')) {
+
+            $manager->remove($product);
+
+            $manager->flush();
+
+            $this->addFlash(
+                'notice',
+                'Product deleted successfully'
+            );
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/delete.html.twig', [
+            'id' => $product->getId(),
+        ]);
+    }
 }
