858c9542c1a50de51ee6ba97e6459251a300ab29
adriancpp <adrian.cpp@wp.pl>
2025-09-06 16:55:15 +0200

    add profiler, showing single product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,6 +15,16 @@ final class ProductController extends AbstractController
     {
         return $this->render('product/index.html.twig', [
             'products' => $products = $repository->findAll(),
+        ]);
+    }
+
+    #[Route('/product/{id<\d+>}', name: 'product_show')]
+    public function show(Product $product): Response
+    {
+
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
