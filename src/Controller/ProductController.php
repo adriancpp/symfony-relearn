d4ae083c7396b32802ba560480ffb94363445f3a
adriancpp <adrian.cpp@wp.pl>
2025-09-06 17:32:03 +0200

    handling forms

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\ProductRepository;
+use App\Form\ProductType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class ProductController extends AbstractController
 {
@@ -21,10 +24,33 @@ final class ProductController extends AbstractController
     #[Route('/product/{id<\d+>}', name: 'product_show')]
     public function show(Product $product): Response
     {
-
-
         return $this->render('product/show.html.twig', [
             'product' => $product
+        ]);
+    }
+
+    #[Route('/product/new', name: 'product_new')]
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $product = new Product;
+
+        $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+
+            $manager->persist($product);
+
+            $manager->flush();
+
+            return $this->redirectToRoute('product_show', [
+                'id' => $product->getId(),
+            ]);
+        }
+
+        return $this->render('product/new.html.twig', [
+            'form' => $form,
         ]);
     }
 }
